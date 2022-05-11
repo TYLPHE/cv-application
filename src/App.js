2e1ab1e211d24fab8a3a1dafc3423b1d5cdbcf6e
TYLPHE <tylphet@gmail.com>
2022-05-11 03:09:44 -0600

    React is very confusing

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'tyler phet',
+      name: 'tyl phe',
       contactInfo: {
         phoneNumber: '1231231234',
         email: 'test@gmail.com',
         github: 'https://github.com/TYLPHE',
         linkedin: 'https://www.linkedin.com/in/tylphe/',
       },
+      skills: 2,
+      projects: 2,
+      experience: 2,
+      education: 3,
     }
 
   }
@@ -36,6 +40,42 @@ class App extends Component {
     this.setState(state);
   }
 
+  handleGithubChange = (e) => {
+    let state = this.state;
+    state.contactInfo.github = e.target.value;
+    this.setState(state);
+  }
+
+  handleLinkedinChange = (e) => {
+    let state = this.state;
+    state.contactInfo.linkedin = e.target.value;
+    this.setState(state);
+  }
+
+  handleProjectsChange = (e) => {
+    let state = this.state;
+    state.projects = e.target.value;
+    this.setState(state);
+  }
+
+  handleSkillsChange = (e) => {
+    let state = this.state;
+    state.skills = e.target.value;
+    this.setState(state);
+  }
+
+  handleExperienceChange = (e) => {
+    let state = this.state;
+    state.experience = e.target.value;
+    this.setState(state);
+  }
+
+  handleEducationChange = (e) => {
+    let state = this.state;
+    state.education = e.target.value;
+    this.setState(state);
+  }
+
   preventDefault(e) {
     e.preventDefault();
   }
@@ -44,6 +84,7 @@ class App extends Component {
     return (
       <div className='app'>
         <form onSubmit={this.preventDefault}>
+          <div className='section-title'>Contact Information</div>
           <label>
             First & Last Name:
             <input 
@@ -69,11 +110,75 @@ class App extends Component {
               onChange={this.handleEmailChange}
             />
           </label>
+
+          <label>
+            GitHub Link: 
+            <input
+              type={'url'}
+              onChange={this.handleGithubChange}
+            />
+          </label>
+
+          <label>
+            LinkedIn Link: 
+            <input
+              type={'url'}
+              onChange={this.handleLinkedinChange}
+            />
+          </label>
+
+          <div className='section-title'>Skills</div>
+          <label>
+            # of skills: 
+            <select onChange={this.handleSkillsChange} defaultValue={2}>
+              <option value={0}>0</option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+            </select>
+          </label>
+
+          <div className='section-title'>Relevant Projects</div>
+          <label>
+            # of Relevant Projects: 
+            <select onChange={this.handleProjectsChange} defaultValue={2}>
+              <option value={0}>0</option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+            </select>
+          </label>
+
+          <div className='section-title'>Relevant Experience</div>
+          <label>
+            # of Relevant Experience: 
+            <select onChange={this.handleExperienceChange} defaultValue={2}>
+              <option value={0}>0</option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+            </select>
+          </label>
+
+          <div className='section-title'>Relevant Education</div>
+          <label>
+            # of Relevant Education: 
+            <select onChange={this.handleEducationChange} defaultValue={3}>
+              <option value={0}>0</option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+            </select>
+          </label>
         </form>
         <DisplayCV 
           name={this.state.name}
           phoneNumber={this.state.contactInfo.phoneNumber}
           email={this.state.contactInfo.email}
+          github={this.state.contactInfo.github}
+          linkedin={this.state.contactInfo.linkedin}
+          projects={this.state.projects}
+          skills={this.state.skills}
+          experience={this.state.experience}
+          education={this.state.education}
         />
       </div>
     );
--- src/DisplayCV.js
+++ src/DisplayCV.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 import './styles/displayCV.css';
 
 class DisplayCV extends Component {
+  constructor() {
+    super();
+    this.state = {
+      window: [],
+      skill: [
+        'Skill #1: ',
+        'Skill #2: ',
+      ]
+    }
+
+    this.editor = this.editor.bind(this);
+  }
+
   splitName(str = '') {
     let fullName = str.split(' ');
     if (str && fullName[1]) {
@@ -14,19 +27,198 @@ class DisplayCV extends Component {
       return `(${str.substring(0, 3)}) ${str.substring(3, 6)}-${str.substring(6)}`
     }
   }
+
+  splitter(info1, info2){
+    if (info1 && info2) {
+      return ` | `;
+    }    
+  }
+  
+  addHyperlink(hyperlink, str) {
+    if(hyperlink && str) {
+      return (
+        <div>
+          <a href={hyperlink} target='_blank' rel="noreferrer">{str}</a>
+        </div>
+      );
+    }
+  }
+
+  createWindow = (e, window) => {
+    console.log(e, window);
+    let state = this.state;
+    state.window = <div>test</div>
+    this.setState(state);
+    console.log(state)
+    // return (window)
+  }
+
+  addSkills = (num) => {
+    function skillDiv(num, obj) {
+      let push = [];
+      for (let i = 0; i < num; i += 1) {
+        let div = (
+          <div key={`skill-${i}`} className='section'>
+            <span className="skill-bold" onClick={(e) => obj.createWindow(e, window)}>{obj.state.skill[i]}</span>
+            <span>Skill description</span>
+            {obj.window}
+          </div>
+        );
+        push = [...push, div];
+      }
+      return push;
+    }
+    let returnValue = <div>{skillDiv(num, this)}</div>
+    if (num > 0) {
+      return (
+        <div>
+          <h1>Skills</h1>
+          {returnValue}
+        </div>
+      )
+    }
+  }
+
+  addProjects(num) {
+    function projDiv(num) {
+      let push = [];
+      for(let i = 1; i <= num; i += 1) {
+        let div = (
+          <div key={`proj-${i}`} className={'section'}>
+            <div>{`Project #${i}`}</div>
+            <ul>
+              <li>point 1</li>
+              <li>point 2</li>
+              <li>point 3</li>
+            </ul>
+          </div>
+        )
+        push = [...push, div];
+      }
+      return push;
+    }
+
+    let returnValue = <div>{projDiv(num)}</div>
+    if (num > 0) {
+      return (
+        <div>
+          <h1>Projects</h1>
+          {returnValue}
+        </div>
+      )
+    }
+  }
+
+  addExperience(num) {
+    function expDiv(num) {
+      let push = [];
+      for (let i = 1; i <= num; i += 1) {
+        let div = (
+          <div key={`exp-${i}`} className={'section'}>
+            <div>{`Job #${i}`}</div>
+            <ul>
+              <li>point 1</li>
+              <li>point 2</li>
+              <li>point 3</li>
+            </ul>
+          </div>
+        );
+        push = [...push, div];
+      }
+      return push;
+    }
+
+    let returnValue = <div>{expDiv(num)}</div>
+    if (num > 0) {
+      return (
+        <div>
+          <h1>Relevant Experience</h1>
+          {returnValue}
+        </div>
+      );
+    }
+  }
+
+  addEducation(num) {
+    function eduDiv(num) {
+      let push = [];
+      for (let i = 1; i <= num; i += 1) {
+        let div = (
+          <div key={`edu-${i}`} className={'section'}>
+            <div className="skill-bold">{`Education #${i}`}</div>
+            <div>Location</div>
+          </div>
+        );
+        push = [...push, div];
+      }
+      return push;
+    }
+
+    let returnValue = <div>{eduDiv(num)}</div>
+    if (num > 0) {
+      return (
+        <div>
+          <h1>Education</h1>
+          {returnValue}
+        </div>
+      );
+    }
+  }
+
+  editor(e) {
+    console.log(e.target.innerText);
+    const value = e.target.innerText;
+    // e.target.remove();
+    return (
+      <input type={'text'} value={value} />
+    )
+  }
+
   render() {
+    const { 
+      name, 
+      email, 
+      phoneNumber, 
+      github, 
+      linkedin,
+      projects,
+      skills,
+      experience,
+      education,
+    } = this.props;
     return (
       <div className="display-cv">
-        <div className="full-name">{this.splitName(this.props.name)}</div>
-
+        <div className="full-name">{this.splitName(name)}</div>
         <div className="contact-info">
-          <div>{this.splitPhone(this.props.phoneNumber)}</div>
-          &nbsp; | &nbsp;
-          <div>{this.props.email}</div>
+          <div>{this.splitPhone(phoneNumber)}</div>
+
+          &nbsp;
+          {this.splitter(email, phoneNumber)}
+          &nbsp;
+
+          {email}
+
+          &nbsp;
+          {this.splitter(email, github)}
+          &nbsp;
+
+          {this.addHyperlink(github, 'GitHub')}
+
+          &nbsp;
+          {this.splitter(github, linkedin)}
+          &nbsp;
+
+          {this.addHyperlink(linkedin, 'LinkedIn')}
         </div>
+        {this.addSkills(skills)}
+        {this.addProjects(projects)}
+        {this.addExperience(experience)}
+        {this.addEducation(education)}
       </div>
     );
   }
+
 }
+
 
 export default DisplayCV;
